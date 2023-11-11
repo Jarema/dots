2a4e6ad84bc41662e40d6cf84c419fdd67df35d2
Jarema <melgaer@gmail.com>
2023-11-11 18:41:18 +0100

    Fix nvim dir
    Signed-off-by: Jarema <melgaer@gmail.com>

---
+++ nvim/after/plugin/fugitive.lua
@@ -0,0 +1,34 @@
+vim.keymap.set("n", "<leader>g", vim.cmd.Git)
+
+local ThePrimeagen_Fugitive = vim.api.nvim_create_augroup("ThePrimeagen_Fugitive", {})
+
+local autocmd = vim.api.nvim_create_autocmd
+autocmd("BufWinEnter", {
+    group = ThePrimeagen_Fugitive,
+    pattern = "*",
+    callback = function()
+        if vim.bo.ft ~= "fugitive" then
+            return
+        end
+
+        local bufnr = vim.api.nvim_get_current_buf()
+        local opts = {buffer = bufnr, remap = false}
+        vim.keymap.set("n", "<leader>p", function()
+            vim.cmd.Git('push')
+        end, opts)
+
+        -- rebase always
+        vim.keymap.set("n", "<leader>P", function()
+            vim.cmd.Git({'pull',  '--rebase'})
+        end, opts)
+
+        -- signed commit
+        vim.keymap.set("n", "<leader>cc", function()
+            vim.cmd.Git('commit -s')
+        end, opts)
+
+        -- NOTE: It allows me to easily set the branch i am pushing and any tracking
+        -- needed if i did not set the branch up correctly
+        vim.keymap.set("n", "<leader>t", ":Git push -u origin ", opts);
+    end,
+})
