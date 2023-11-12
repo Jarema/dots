ec180339dfd126490fffd09663e38a7a7716316d
Jarema <melgaer@gmail.com>
2023-11-12 15:34:48 +0100

    Update nvim config
    Signed-off-by: Jarema <melgaer@gmail.com>

--- nvim/after/plugin/colors.lua
+++ nvim/after/plugin/colors.lua
@@ -2,13 +2,12 @@ require('rose-pine').setup({
     disable_background = true
 })
 
-function ColorMyPencils(color) 
-	color = color or "rose-pine"
-	vim.cmd.colorscheme(color)
+function ColorMyPencils(color)
+    color = color or "rose-pine"
+    vim.cmd.colorscheme(color)
 
-	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
 
 ColorMyPencils()
--- nvim/lua/jarema/packer.lua
+++ nvim/lua/jarema/packer.lua
@@ -92,4 +92,8 @@ return require('packer').startup(function(use)
             })
         end
     })
+    use {
+        'nvim-lualine/lualine.nvim',
+        requires = { 'nvim-tree/nvim-web-devicons', opt = true }
+    }
 end)
