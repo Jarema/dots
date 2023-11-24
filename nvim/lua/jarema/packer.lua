b36d70c9483aed5f4a06ec3626916952416a973b
Jarema <melgaer@gmail.com>
2023-11-24 13:33:23 +0100

    Update packer and skhd
    Signed-off-by: Jarema <melgaer@gmail.com>

--- nvim/lua/jarema/packer.lua
+++ nvim/lua/jarema/packer.lua
@@ -117,4 +117,17 @@ return require('packer').startup(function(use)
         after = "nvim-treesitter",
         requires = "nvim-treesitter/nvim-treesitter",
     })
+    use {
+        'xbase-lab/xbase',
+        run = 'make install', -- or "make install && make free_space" (not recommended, longer build time)
+        requires = {
+            "neovim/nvim-lspconfig",
+            -- "nvim-telescope/telescope.nvim", -- optional
+            -- "nvim-lua/plenary.nvim", -- optional/requirement of telescope.nvim
+            -- "stevearc/dressing.nvim", -- optional (in case you don't use telescope but something else)
+        },
+        config = function()
+            require 'xbase'.setup({}) -- see default configuration bellow
+        end
+    }
 end)
