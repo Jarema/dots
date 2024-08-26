4f13fc6acb9d85af9e7fbb2d9774e68ac78caca2
Jarema <melgaer@gmail.com>
2024-08-26 13:28:27 +0200

    Migrate to lazy

--- nvim/after/plugin/zenmode.lua
+++ nvim/after/plugin/zenmode.lua
@@ -1,16 +1,14 @@
-
 vim.keymap.set("n", "<leader>zz", function()
     require("zen-mode").setup {
         window = {
             width = 90,
-            options = { }
+            options = {}
         },
     }
     require("zen-mode").toggle()
     vim.wo.wrap = false
     vim.wo.number = true
     vim.wo.rnu = true
-    ColorMyPencils()
 end)
 
 
@@ -18,7 +16,7 @@ vim.keymap.set("n", "<leader>zZ", function()
     require("zen-mode").setup {
         window = {
             width = 80,
-            options = { }
+            options = {}
         },
     }
     require("zen-mode").toggle()
@@ -26,5 +24,4 @@ vim.keymap.set("n", "<leader>zZ", function()
     vim.wo.number = false
     vim.wo.rnu = false
     vim.opt.colorcolumn = "0"
-    ColorMyPencils()
 end)
--- nvim/lua/jarema/init.lua
+++ nvim/lua/jarema/init.lua
@@ -32,3 +32,143 @@ vim.g.netrw_browse_split = 0
 vim.g.netrw_banner = 0
 vim.g.netrw_winsize = 25
 vim.opt.shell = "/bin/bash"
+
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+    local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+    if vim.v.shell_error ~= 0 then
+        vim.api.nvim_echo({
+            { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+            { out,                            "WarningMsg" },
+            { "\nPress any key to exit..." },
+        }, true, {})
+        vim.fn.getchar()
+        os.exit(1)
+    end
+end
+vim.opt.rtp:prepend(lazypath)
+
+require('lazy').setup({
+    {
+        'nvim-telescope/telescope.nvim',
+        tag = '0.1.4',
+        dependencies = { { 'nvim-lua/plenary.nvim' } }
+    },
+    {
+        'rose-pine/neovim',
+        as = 'rose-pine',
+        config = function()
+            vim.cmd('colorscheme rose-pine')
+        end
+    },
+    {
+        "nvim-telescope/telescope-file-browser.nvim",
+        dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" }
+    },
+    {
+        "folke/trouble.nvim",
+        config = function()
+            require("trouble").setup {
+                icons = false,
+                -- your configuration comes here
+                -- or leave it empty to use the default settings
+                -- refer to the configuration section below
+            }
+        end
+    },
+    {
+        'nvim-treesitter/nvim-treesitter',
+        build = function()
+            local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+            ts_update()
+        end,
+        dependencies = {
+            "nvim-treesitter/nvim-treesitter-textobjects",
+            "nvim-treesitter/playground",
+            "nvim-treesitter/nvim-treesitter-context",
+        }
+    },
+    "theprimeagen/harpoon",
+    "theprimeagen/refactoring.nvim",
+    "mbbill/undotree",
+    "tpope/vim-fugitive",
+    {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v1.x',
+        dependencies = {
+            -- LSP Support
+            { 'neovim/nvim-lspconfig' },
+            { 'williamboman/mason.nvim' },
+            { 'williamboman/mason-lspconfig.nvim' },
+
+            -- Autocompletion
+            { 'hrsh7th/nvim-cmp' },
+            { 'hrsh7th/cmp-buffer' },
+            { 'hrsh7th/cmp-path' },
+            { 'saadparwaiz1/cmp_luasnip' },
+            { 'hrsh7th/cmp-nvim-lsp' },
+            { 'hrsh7th/cmp-nvim-lua' },
+
+            -- Snippets
+            { 'L3MON4D3/LuaSnip' },
+            { 'rafamadriz/friendly-snippets' },
+        }
+    },
+    "lewis6991/gitsigns.nvim",
+    "github/copilot.vim",
+    "eandrju/cellular-automaton.nvim",
+    "laytan/cloak.nvim",
+    "rhysd/vim-fixjson",
+    "sindrets/diffview.nvim",
+    "tpope/vim-commentary",
+    {
+        "windwp/nvim-autopairs",
+        config = function() require("nvim-autopairs").setup {} end
+    },
+    {
+        "kylechui/nvim-surround",
+        version = "*", -- Use for stability; omit to use `main` branch for the latest features
+        event = "VeryLazy",
+        config = function()
+            require("nvim-surround").setup({
+                -- Configuration here, or leave empty to use defaults
+            })
+        end
+    },
+    {
+        'nvim-lualine/lualine.nvim',
+        dependencies = { 'nvim-tree/nvim-web-devicons', opt = true }
+    },
+    {
+        'echasnovski/mini.nvim',
+        branch = 'stable',
+        dependencies = { 'nvim-tree/nvim-web-devicons', opt = true }
+    },
+    {
+        "folke/which-key.nvim",
+        config = function()
+            vim.o.timeout = true
+            vim.o.timeoutlen = 300
+            require("which-key").setup {
+                -- your configuration comes here
+                -- or leave it empty to use the default settings
+                -- refer to the configuration section below
+            }
+        end
+    },
+    {
+        'xbase-lab/xbase',
+        build = 'make install', -- or "make install && make free_space" (not recommended, longer build time)
+        dependencies = {
+            "neovim/nvim-lspconfig",
+            -- "nvim-telescope/telescope.nvim", -- optional
+            -- "nvim-lua/plenary.nvim", -- optional/requirement of telescope.nvim
+            -- "stevearc/dressing.nvim", -- optional (in case you don't use telescope but something else)
+        },
+        config = function()
+            require 'xbase'.setup({}) -- see default configuration bellow
+        end
+    },
+})
